63ea11f1b8ad682817be761a505771145062fd52
Kamran <kamrankek48@gmail.com>
2025-01-19 16:01:25 +0400

    Названия приложений

--- web/apps/chats/apps.py
+++ web/apps/chats/apps.py
@@ -4,3 +4,4 @@ from django.apps import AppConfig
 class ChatsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'web.apps.chats'
+    verbose_name = '”правление группами'
